018271610437dc805d3366c42bc807490e4e8e65
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-11-17 13:07:32 +0800

    :sparkles: put: 上传 文件夹: 不使用全局变量

--- put/folder.go
+++ put/folder.go
@@ -11,15 +11,6 @@ import (
 	"time"
 )
 
-var (
-	accessKey  string
-	secretKey  string
-	endpoint   string
-	bucketName string
-	uri        string
-	folder     string
-)
-
 // PutFolderCommand 上传 文件夹
 func PutFolderCommand() *cli.Command {
 	return &cli.Command{
@@ -27,12 +18,12 @@ func PutFolderCommand() *cli.Command {
 		Usage: "上传 文件夹",
 		Flags: append(common.CommonFlagRequired(), common.UriFlag(true), common.FolderFlag(true)),
 		Action: func(context *cli.Context) error {
-			accessKey = context.String(common.AccessKey)
-			secretKey = context.String(common.SecretKey)
-			endpoint = context.String(common.Endpoint)
-			bucketName = context.String(common.BucketName)
-			uri = context.String(common.Uri)
-			folder = context.String(common.Folder)
+			var accessKey = context.String(common.AccessKey)
+			var secretKey = context.String(common.SecretKey)
+			var endpoint = context.String(common.Endpoint)
+			var bucketName = context.String(common.BucketName)
+			var uri = context.String(common.Uri)
+			var folder = context.String(common.Folder)
 
 			fileInfo, err := os.Stat(folder)
 			if err != nil {
@@ -40,7 +31,7 @@ func PutFolderCommand() *cli.Command {
 			}
 
 			if fileInfo.IsDir() {
-				return PutObjectFromFolder(folder)
+				return PutObjectFromFolder(accessKey, secretKey, endpoint, bucketName, uri, folder)
 			} else {
 				return errors.New(fmt.Sprintf("路径 %s 不是一个文件夹", folder))
 			}
@@ -48,52 +39,56 @@ func PutFolderCommand() *cli.Command {
 	}
 }
 
-func PutObjectFromFolder(folder string) error {
+func PutObjectFromFolder(accessKey string, secretKey string, endpoint string, bucketName string, uri string, folder string) error {
 
 	start := time.Now()
 	log.Printf("上传 文件夹 开始")
 
-	err := filepath.WalkDir(folder, VisitFile)
+	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			// 跳过文件夹
+			return nil
+		}
+
+		fileName := filepath.Base(path)
+
+		if fileName == "desktop.ini" {
+			return nil
+		}
+
+		file := path[len(folder)+1:]
+
+		log.Printf("上传 文件 %s 开始", path)
+
+		bucket, err := common.GetBucket(accessKey, secretKey, endpoint, bucketName)
+		if err != nil {
+			return err
+		}
+
+		objectKey := uri + "/" + file
+
+		// Upload an object with local file name, user need not open the file.
+		err = bucket.PutObjectFromFile(objectKey, path)
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("上传 文件 %s 结束", path)
+
+		return nil
+	})
+
 	if err != nil {
 		return err
 	}
 
 	log.Printf("上传 文件夹 结束（%s）", time.Since(start))
-
-	return nil
-}
-
-func VisitFile(path string, info os.DirEntry, err error) error {
-	if info.IsDir() {
-		// 跳过文件夹
-		return nil
-	}
-
-	fileName := filepath.Base(path)
-
-	if fileName == "desktop.ini" {
-		return nil
-	}
-
-	file := path[len(folder)+1:]
-
-	log.Printf("上传 文件 %s 开始", path)
-
-	bucket, err := common.GetBucket(accessKey, secretKey, endpoint, bucketName)
-	if err != nil {
-		return err
-	}
-
-	objectKey := uri + "/" + file
-
-	// Upload an object with local file name, user need not open the file.
-	err = bucket.PutObjectFromFile(objectKey, path)
-
-	if err != nil {
-		return err
-	}
-
-	log.Printf("上传 文件 %s 结束", path)
 
 	return nil
 }
